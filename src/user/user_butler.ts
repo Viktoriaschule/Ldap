75483b6663ce7f97f53c100fc8e0140c508ccb03
Finn <tupfi2@gmx.de>
2020-02-08 13:17:43 +0100

    Added user name request

--- src/user/user_butler.ts
+++ src/user/user_butler.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import getAuth from '../utils/auth';
-import { checkUser } from '../authentication/ldap_client';
+import { checkUser, getFullName } from '../authentication/ldap_client';
 
 const userRouter = express.Router();
 
 userRouter.get('/:username', async (req, res) => {
     const status = await checkUser(req.params.username, getAuth(req));
     res.json(status);
+});
+
+userRouter.get('/name/:username', async (req, res) => {
+    const name = await getFullName(req.params.username, getAuth(req));
+    res.json({name: name});
 });
 
 export default userRouter;
